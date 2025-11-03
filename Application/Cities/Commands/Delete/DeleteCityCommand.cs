6678a52a89c9209f0a9fde58fe488d572e972127
mustafa-fathy <mostafafathy997@gmail.com>
2025-11-03 13:37:27 +0200

    Adding Delete Command

---
+++ Application/Cities/Commands/Delete/DeleteCityCommand.cs
@@ -0,0 +1,39 @@
+﻿using Application.Common.Dtos;
+using Application.Interfaces;
+using MediatR;
+using Microsoft.Identity.Client;
+using SendGrid.Helpers.Errors.Model;
+
+namespace Application.Cities.Commands.Delete
+{
+    public class DeleteCityCommand : IRequest<ResponseDto<object>>
+    {
+        public int Id { get; set; }
+        public string? CurrentUserId { get; set; }
+
+        public class DeleteCityHandler : IRequestHandler<DeleteCityCommand, ResponseDto<object>>
+        {
+            private readonly IAppDbContext _dbContext;
+
+            public DeleteCityHandler(IAppDbContext dbContext)
+            {
+                _dbContext = dbContext;
+            }
+            public async Task<ResponseDto<object>> Handle(DeleteCityCommand request, CancellationToken cancellationToken)
+            {
+                var city = await _dbContext.Cities.FindAsync(request.Id) ?? throw new NotFoundException("City Not Found!");
+                city.Deleted = true;
+                city.ModifiedById = request.CurrentUserId;
+                city.ModificationDate = DateTime.Now;
+                await _dbContext.SaveChangesAsync(cancellationToken);
+                return ResponseDto<object>.Success(new ResultDto
+                {
+
+                    Message = "Deleted Success",
+
+                });
+
+            }
+        }
+    }
+}
--- Application/Cities/Commands/Edit/UpdateCityCommand.cs
+++ Application/Cities/Commands/Edit/UpdateCityCommand.cs
@@ -5,49 +5,52 @@ using SendGrid.Helpers.Errors.Model;
 
 namespace Application.Cities.Commands.Edit
 {
-    public class UpdateCityCommand :IRequest<ResponseDto<object>>
+    public class UpdateCityCommand : IRequest<ResponseDto<object>>
     {
         public int Id { get; set; }
         public string? NameAr { get; set; }
         public string? NameEn { get; set; }
         public int? CountryId { get; set; }
         public string? CurrentUserId { get; set; }
-
-        private readonly IAppDbContext _dbContext;
-        public UpdateCityCommand(IAppDbContext dbContext)
+        public class UpdateCityHandler : IRequestHandler<UpdateCityCommand, ResponseDto<object>>
         {
-            _dbContext = dbContext;
-        }
-        public async Task<ResponseDto<object>> Handle(UpdateCityCommand request, CancellationToken cancellationToken)
-        {
-            var city = await _dbContext.Cities.FindAsync(request.Id) ?? throw new NotFoundException("City Not Found.");
 
-            city.NameEn = request.NameEn ?? city.NameEn;
-
-            city.NameAr = request.NameAr ?? city.NameAr;
-
-            city.CountryId = request.CountryId ?? city.CountryId;
-
-            city.ModifiedById = request.CurrentUserId ;
-
-            city.ModificationDate = DateTime.UtcNow;
-
-            await _dbContext.SaveChangesAsync(cancellationToken);
-
-            return ResponseDto<object>.Success(new ResultDto
+            private readonly IAppDbContext _dbContext;
+            public UpdateCityHandler(IAppDbContext dbContext)
             {
-                Message = "Updates Approved.",
-                Result = new
+                _dbContext = dbContext;
+            }
+            public async Task<ResponseDto<object>> Handle(UpdateCityCommand request, CancellationToken cancellationToken)
+            {
+                var city = await _dbContext.Cities.FindAsync(request.Id) ?? throw new NotFoundException("City Not Found.");
+
+                city.NameEn = request.NameEn ?? city.NameEn;
+
+                city.NameAr = request.NameAr ?? city.NameAr;
+
+                city.CountryId = request.CountryId ?? city.CountryId;
+
+                city.ModifiedById = request.CurrentUserId;
+
+                city.ModificationDate = DateTime.UtcNow;
+
+                await _dbContext.SaveChangesAsync(cancellationToken);
+
+                return ResponseDto<object>.Success(new ResultDto
                 {
-                    CityId=city.Id
-                }
+                    Message = "Updates Approved.",
+                    Result = new
+                    {
+                        CityId = city.Id
+                    }
 
-            });
+                });
 
 
 
 
 
+            }
         }
     }
 }
