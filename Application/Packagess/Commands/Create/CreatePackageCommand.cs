eb81cf6206d7fdcff01269fa25d3fa1553f7b9fc
mustafa-fathy <mostafafathy997@gmail.com>
2025-11-19 11:48:04 +0200

    Editing1

---
+++ Application/Packagess/Commands/Create/CreatePackageCommand.cs
@@ -0,0 +1,22 @@
+﻿using Application.Common.Dtos;
+using Application.Interfaces;
+using MediatR;
+
+namespace Application.Packagess.Commands.Create
+{
+    public class CreatePackageCommand : IRequest<ResponseDto<object>>
+    {
+        public class CreatePackageHandler : IRequestHandler<CreatePackageCommand , ResponseDto<object>>
+        {
+            private readonly IAppDbContext _dbContext;
+            public CreatePackageHandler(IAppDbContext dbContext)
+            {
+                _dbContext = dbContext;
+            }
+            public async Task<ResponseDto<object>> Handle (CreatePackageCommand request , CancellationToken cancellationToken)
+            {
+
+            }
+        }
+    }
+}
